55131b1ee3c8f4f306916a288d1d58525f4854a2
SpectreAscended <94819487+SpectreAscended@users.noreply.github.com>
2022-04-27 17:41:45 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const input = document.querySelector('.todo-input');
+const postButton = document.querySelector('.form-button-post')
+const delButton = document.querySelector('.form-button-delete')
+const list = document.querySelector('.list--list')
+
+
+
+
+const init = function() {
+    const postList = function() {
+        const html = `<li class="list-item">${input.value}</li>`
+        list.insertAdjacentHTML('beforeend', html)
+        input.value = '';
+    };
+    
+    postButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        postList();
+    });
+    
+    input.addEventListener('keydown', function(e) {
+        if(e.key === 'Enter'){
+            postList();
+        };
+    });
+    
+    delButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        list.innerHTML = '';
+    });
+};
+
+init();
