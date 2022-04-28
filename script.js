48179c7c6acbe2858f75d3405b4049e7a0eaa45e
SpectreAscended <94819487+SpectreAscended@users.noreply.github.com>
2022-04-27 21:45:05 -0600

    Fixed bug, added responsiveness

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 "use strict";
 
-const input = document.querySelector('.todo-input');
-const postButton = document.querySelector('.form-button-post')
-const delButton = document.querySelector('.form-button-delete')
-const list = document.querySelector('.list--list')
+
 
 const app = function() {
+    const input = document.querySelector('.todo-input');
+    const postButton = document.querySelector('.form-button-post');
+    const clearButton = document.querySelector('.form-button-delete');
+    const delItem = document.querySelectorAll('.del-btn')
+    const list = document.querySelector('.list--list');
+    const item = document.querySelectorAll('.list-item')
+    const listContainer = document.querySelector('.todo__list')
+
     const postList = function() {
         if(!input.value) return;
-        const html = `<li class="list-item">${input.value}</li>`;
+        const html = `<li class="list-item">${input.value}<button class="del-btn">X</button></li>`;
         list.insertAdjacentHTML('beforeend', html);
         input.value = '';
+        
     };
     
     postButton.addEventListener('click', function(e) {
@@ -20,13 +26,20 @@ const app = function() {
     
     input.addEventListener('keydown', function(e) {
         if(e.key === 'Enter'){
+            e.preventDefault();
             postList();
         };
     });
     
-    delButton.addEventListener('click', function(e) {
+    clearButton.addEventListener('click', function(e) {
         e.preventDefault();
         list.innerHTML = '';
+    });
+
+    listContainer.addEventListener('click', function(e) {
+        const clicked = e.target.closest('.del-btn');
+        if(!clicked) return;
+        clicked.closest('li').innerHTML = '';
     });
 };
 
